13d7b2d5c69ce6b67e137c8fa766774fe41ab8b4
LettaVio <viollogika@gmail.com>
2022-07-11 11:56:14 +0300

    react first app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import Header from "./Header.jsx";
+import Body from "./Body.jsx";
+import Footer from "./Footer.jsx";
+import "@animxyz/core";
+import { XyzTransition } from "@animxyz/react";
+//import {HashRouter} from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <XyzTransition appear duration="auto">
+      <div className="page-wrap">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    </XyzTransition>
   );
 }
-
-export default App;
+// const NewApp = (props) => {
+//   return  <HashRouter basename={process.env.PUBLIC_URL}>
+//       <Provider store={store}>
+//           <AppContainer />
+//       </Provider>
+//     </HashRouter>
+//  }
