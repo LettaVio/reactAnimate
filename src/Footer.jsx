6d8bddb6551bfc7cb5874b172feae4bca852398a
LettaVio <viollogika@gmail.com>
2022-07-11 15:56:10 +0300

    info cv

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,15 +1,23 @@
+import {FaGithubSquare, FaFacebookSquare, FaLinkedin} from 'react-icons/fa';
+import {MdEmail} from 'react-icons/md';
+
 export default function Footer() {
     return (
-      <div className="page-footer" xyz="fade down ease-in-out delay-10">
-        <div
-          className="footer-logo xyz-nested"
-          xyz="fade left ease-in-out delay-10"
-        ></div>
+      <footer className="page-footer" xyz="fade down ease-in-out delay-10">
+
+        <div className="footer-logo xyz-nested" xyz="fade left ease-in-out delay-10" >
+       </div>
         <div className="footer-right" xyz="fade up stagger ease-in-out delay-10">
-          <div className="footer-item xyz-nested"></div>
-          <div className="footer-item xyz-nested"></div>
-          <div className="footer-item xyz-nested"></div>
+            <a className="footer-item xyz-nested" href="#" target={'_blank'}>
+                <FaLinkedin/>
+            </a>
+            <a className="footer-item xyz-nested " href="https://github.com/LettaVio?tab=repositories" target={'_blank'}>
+                <FaGithubSquare/>
+            </a> 
+            <a className="footer-item xyz-nested" href='#' target={'_blank'}>
+                <FaFacebookSquare />
+            </a>
         </div>
-      </div>
+      </footer>
     );
   }
