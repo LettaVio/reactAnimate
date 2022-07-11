bdc7fed3f1a829452b3885fc636c0adf94db26be
LettaVio <viollogika@gmail.com>
2022-07-11 16:39:11 +0300

    deploy

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -8,13 +8,13 @@ export default function Footer() {
         <div className="footer-logo xyz-nested" xyz="fade left ease-in-out delay-10" >
        </div>
         <div className="footer-right" xyz="fade up stagger ease-in-out delay-10">
-            <a className="footer-item xyz-nested" href="#" target={'_blank'}>
+            <a className="footer-item xyz-nested" href="https://www.linkedin.com/in/violetta-gryshchuk/" target={'_blank'}>
                 <FaLinkedin/>
             </a>
             <a className="footer-item xyz-nested " href="https://github.com/LettaVio?tab=repositories" target={'_blank'}>
                 <FaGithubSquare/>
             </a> 
-            <a className="footer-item xyz-nested" href='#' target={'_blank'}>
+            <a className="footer-item xyz-nested" href='https://www.facebook.com/LoreleiVioletta' target={'_blank'}>
                 <FaFacebookSquare />
             </a>
         </div>
